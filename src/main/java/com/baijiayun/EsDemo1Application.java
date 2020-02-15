7f52728088c45662d269d6fb0a961d1b1730989a
万引胜 <wan_yinsheng@163.com>
2020-02-15 13:54:59 +0800

    y98yb

---
+++ src/main/java/com/baijiayun/EsDemo1Application.java
@@ -0,0 +1,15 @@
+package com.baijiayun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsDemo1Application {
+
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(EsDemo1Application.class, args);
+    }
+
+}
